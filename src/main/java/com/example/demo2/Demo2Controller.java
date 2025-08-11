e16acedb428a03963cf2b6aea939a07791767611
Abirami <abirami.rajalingam@gmail.com>
2025-08-11 07:47:01 -0500

    first commit

---
+++ src/main/java/com/example/demo2/Demo2Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo2Controller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Azure!";
+    }
+}
